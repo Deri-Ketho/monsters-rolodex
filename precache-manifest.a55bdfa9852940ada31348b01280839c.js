d892d5cb483a4772b6e8f9f946c5d174885d7757
gaspard <gaspard@email.com>
2020-04-27 13:57:28 +0200

    Updates

---
+++ precache-manifest.a55bdfa9852940ada31348b01280839c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c3c751b1d02860d9a9b43d75c8de47a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "792213750e485b346c9b",
+    "url": "/monsters-rolodex/static/css/main.b5c373fa.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "792213750e485b346c9b",
+    "url": "/monsters-rolodex/static/js/main.cdd2335f.chunk.js"
+  },
+  {
+    "revision": "0c81cb278443448182f0",
+    "url": "/monsters-rolodex/static/js/runtime-main.165419b0.js"
+  }
+]);
